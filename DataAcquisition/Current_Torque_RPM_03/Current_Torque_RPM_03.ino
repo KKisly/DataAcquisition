91b0d1ae090184423bd2d20e3b67b49b2711942b
KKisly <kisly.k@gmail.com>
2017-03-05 15:25:28 -0800

    Update Current_Torque_RPM_03.ino

--- DataAcquisition/Current_Torque_RPM_03/Current_Torque_RPM_03.ino
+++ DataAcquisition/Current_Torque_RPM_03/Current_Torque_RPM_03.ino
@@ -1,5 +1,6 @@
 /*Constantine Kisly
-ACS758 Current, Torgue and RPM Measurement Script for Data acqusition station*/
+ACS758 Current, Torgue and RPM Measurement Script for Data acqusition station
+*/
 #include "HX711.h"
 //#define DEBUG
 //#include <SPI.h>
